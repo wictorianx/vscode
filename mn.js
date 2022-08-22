0b0c298d539bef5dc8f6e39a341fa28ac505e54f
wictorianx <88106599+wictorianx@users.noreply.github.com>
2022-08-23 01:22:28 +0300

    3

--- mn.js
+++ mn.js
@@ -1,8 +1,40 @@
 
 const cardArray = [
     {
-        
-
-
-    }
+        name:"alien",
+        img: "alien.jpg"
+    },
+    {
+        name:"research",
+        img: "research.jpg"
+    },
+    {
+        name:"rocket",
+        img: "rocket.png"
+    },
+    {
+        name:"satellite",
+        img: "satellite.png"
+    },
+    {
+        name:"energy",
+        img: "energy.jpg"
+    },
+    {
+        name:"astreoid mine",
+        img: "astreoid mine.jpg"
+    },
 ]
+cardArray.sort(() => 0.5-Math.random())
+const griD = document.querySelector('#grid')
+
+function createBoard (){
+    for (let i = 0; i<10; i++)
+    const card = document.createElement('img')
+    card.setAttribute('src',"alien.jpg")
+    card.setAttribute('data-id',i)
+    griD.appendChild(card)
+
+
+
+}
