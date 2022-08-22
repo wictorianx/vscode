bc478012b3338e6a615ead4f4766bc063af46810
wictorianx <88106599+wictorianx@users.noreply.github.com>
2022-08-23 01:30:31 +0300

    add par

--- mn.js
+++ mn.js
@@ -29,11 +29,11 @@ cardArray.sort(() => 0.5-Math.random())
 const griD = document.querySelector('#grid')
 
 function createBoard (){
-    for (let i = 0; i<10; i++)
+    for (let i = 0; i<10; i++){
     const card = document.createElement('img')
     card.setAttribute('src',"alien.jpg")
     card.setAttribute('data-id',i)
-    griD.appendChild(card)
+    griD.appendChild(card)}
 
 
 
