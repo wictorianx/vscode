6a0d6a1dec6206600408afa9051ba9521ba72729
Hazal Kaya <88106599+wictorianx@users.noreply.github.com>
2022-08-23 17:28:23 +0000

    final

--- mn.js
+++ mn.js
@@ -20,8 +20,8 @@ const cardArray = [
         img: "energy.jpg"
     },
     {
-        name:"astreoid mine",
-        img: "astreoid mine.jpg"
+        name:"astreoid_mine",
+        img: "astreoid_mine.jpg"
     },
     {
         name:"alien",
@@ -44,15 +44,16 @@ const cardArray = [
         img: "energy.jpg"
     },
     {
-        name:"astreoid mine",
-        img: "astreoid mine.jpg"
+        name:"astreoid_mine",
+        img: "astreoid_mine.jpg"
     },
 ]
 cardArray.sort(() => 0.5-Math.random())
+const resultDisplay = document.querySelector("#result")
 const griD = document.querySelector('#grid')
-const cardsChosen = []
-const cardsChosenIds = []
-
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
 
 function createBoard (){
     for (let i = 0; i<cardArray.length; i++){
@@ -64,25 +65,41 @@ function createBoard (){
 }
 createBoard()
 function checkMatch(){ 
-    let cards = document.querySelectorAll("#grid img")
-    if (cardsChosen[0]===cardsChosen[1]){
-        alert("found match")
-
-
+    let win = true
+    let cards = document.querySelectorAll("img")
+    if (cardsChosenIds[0] === cardsChosenIds[1]){
+      alert("hackerman?")
+      win = false
     }
+    if (cardsChosen[0]===cardsChosen[1] && win){
+      alert("found match")
+      cards[cardsChosenIds[0]].setAttribute("src","R.jfif")
+      cards[cardsChosenIds[1]].setAttribute("src","R.jfif")
+      cards[cardsChosenIds[0]].removeEventListener("click",flipCard)
+      cards[cardsChosenIds[1]].removeEventListener("click",flipCard)
+      cardsWon.push(cardsChosen)
+    }
+    else{
+      cards[cardsChosenIds[0]].setAttribute("src","blue_giant.jpg")
+      cards[cardsChosenIds[1]].setAttribute("src","blue_giant.jpg")
+      alert("try again")
+    }
+    cardsChosen = []
+    cardsChosenIds = []
+    
+
+    if (cardsWon.length === cardArray.length/2){
+      resultDisplay.innerHTML = "Won"}
+    else{resultDisplay.textContent = `${cardsWon.lenght*2}/${cardArray.length}`
+}
 }
 function flipCard(){
     let cardId = this.getAttribute("data-id")
     cardsChosen.push(cardArray[cardId].name)
     cardsChosenIds.push(cardId)
-    this.setAttribute("src",cardArray[cardId.img])
+    this.setAttribute("src",cardArray[cardId].img)
+
     if (cardsChosen.length === 2){
         setTimeout(checkMatch,500)
     }
-
-
-
-
-
-
 }
